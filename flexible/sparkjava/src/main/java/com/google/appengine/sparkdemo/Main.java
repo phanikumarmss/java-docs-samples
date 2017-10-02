9468a5d94d48e60215cae01f2405d05537f14490
T.V.R.L.PhaniKumar Dadi <pdadi@miraclesoft.com>
2017-10-02 19:24:08 +0530

    Update Main.java

--- flexible/sparkjava/src/main/java/com/google/appengine/sparkdemo/Main.java
+++ flexible/sparkjava/src/main/java/com/google/appengine/sparkdemo/Main.java
@@ -20,7 +20,7 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        port(8080);
+        //port(8080);
         get("/", (req, res) -> "Hello World");
     }
 }
